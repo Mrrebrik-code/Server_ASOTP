5be57c880fb67ae6acf1bbb0dc6d10ba162e466c
Mrrebrik <itibsoft@gmail.com>
2021-11-14 12:46:38 +0700

    Create system Vector3 => normailzed, magnitude, distance, ToString

---
+++ classes/core/Vector3.js
@@ -0,0 +1,30 @@
+module.exports = class Vector3 {
+    constructor() {
+        this.x;
+        this.y;
+        this.z;
+    }
+
+    magnitude() {
+        return Math.sqrt((this.x * this.x) + (this.y * this.y) + (this.z * this.z));
+    }
+
+    normalized() {
+        var magnitudeVector3 = this.magnitude();
+        return new Vector3(this.x / magnitudeVector3, this.y / magnitudeVector3, this.z * magnitudeVector3);
+    }
+
+    distance(OtherVector = Vector3) {
+        var direction = new Vector3();
+        direction.x = OtherVector.x - this.x;
+        direction.y = OtherVector.y - this.y;
+        direction.z = OtherVector.z - this.z;
+
+        return direction.magnitude();
+    }
+
+    ToString() {
+        return `X:${this.x}, Y:${this.y}, Z${this.z}`;
+    }
+
+}
